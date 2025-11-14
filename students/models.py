a5921b41f3b7519d4edb4c9bcd1bc7a050f7a16f
SimoneChiodo <simochi.005@gmail.com>
2025-11-14 18:13:45 +0100

    Change branch size in Student model; Migrate to db

--- students/models.py
+++ students/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Student(models.Model):
   student_id = models.CharField(max_length=10)
   name = models.CharField(max_length=50)
-  branch = models.CharField(max_length=20)
+  branch = models.CharField(max_length=50)
 
   def __str__(self):
     return self.name
