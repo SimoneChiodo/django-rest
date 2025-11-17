91204d02883ccc0d709e34f2399f1a620200f38d
SimoneChiodo <simochi.005@gmail.com>
2025-11-17 16:45:05 +0100

    Add employee post route

--- api/views.py
+++ api/views.py
@@ -50,7 +50,16 @@ def studentDetailView(request, pk):
 
 # EMPLOYEES -------------------------
 class Employees(APIView):
+  # INDEX
   def get(self, request):
     employees = Employee.objects.all()
     serializer = EmployeeSerializer(employees, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+  
+  # CREATE
+  def post(self, request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
