65d18bc15304d0fa59745635e9edd399ba266056
SimoneChiodo <simochi.005@gmail.com>
2025-11-25 16:07:26 +0100

    Add id range filter field

--- employees/filters.py
+++ employees/filters.py
@@ -4,7 +4,8 @@ from .models import Employee
 class EmployeeFilter(django_filters.FilterSet):
   designation = django_filters.CharFilter(field_name='designation', lookup_expr='iexact') # iexact: il risultato deve essere uguale alla ricerca (specificare una 'lookup_expr' rende il campo Case-Unsensitive)
   emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains') # icontains: puù anche solo contenere
+  id = django_filters.RangeFilter(field_name='id') # icontains: puù anche solo contenere
 
   class Meta:
     model = Employee
-    fields = ['designation']
+    fields = ['designation', 'emp_name', 'id']
